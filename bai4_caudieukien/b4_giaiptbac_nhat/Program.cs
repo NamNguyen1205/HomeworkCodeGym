b0b8a2748ac996cfa30301d0fe14a883f2cbc425
nguyennam <nguyensonnam1205@gmail.com>
2025-07-15 16:29:20 +0700

    add
    add

---
+++ bai4_caudieukien/b4_giaiptbac_nhat/Program.cs
@@ -0,0 +1,28 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //pt bac 1: ax + b = 0
+        Console.WriteLine("Nhap gia tri a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap gia tri b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Pt vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Pt vo nghiem");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            System.Console.WriteLine("phuong trinh co nghiem la: " + x);
+        }
+    }
+}
