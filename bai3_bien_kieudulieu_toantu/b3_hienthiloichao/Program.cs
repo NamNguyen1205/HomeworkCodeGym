c95e948bd5323c3f001c44b048889b9f44703e8a
nguyennam <nguyensonnam1205@gmail.com>
2025-07-15 16:07:17 +0700

    adđ
    add

---
+++ bai3_bien_kieudulieu_toantu/b3_hienthiloichao/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string? name = Convert.ToString(Console.ReadLine()); //co the null
+        Console.WriteLine("Hello: " + name);
+    }
+}
