ddd1d7d9d7948d2ca57b69c79190aad4e5a129d5
nguyennam <nguyensonnam1205@gmail.com>
2025-07-15 16:16:38 +0700

    add
    add

---
+++ bai3_bien_kieudulieu_toantu/b3_chuyendoitiente/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int rate = 23000;
+
+        Console.WriteLine("Nhap USD muon chuyen doi: ");
+        int usd = Convert.ToInt32(Console.ReadLine());
+
+        int vnd = rate*usd;
+        Console.WriteLine("VND tuong ung la: " + vnd);
+
+    }
+}
