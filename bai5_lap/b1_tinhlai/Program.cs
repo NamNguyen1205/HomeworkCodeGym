918669559c5113d1c34ef25093321fc2a759347b
nguyennam <nguyensonnam1205@gmail.com>
2025-07-16 21:04:37 +0700

    add
    add

---
+++ bai5_lap/b1_tinhlai/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double money = 1.0;
+        int month = 1;
+        double interest_rate = 1.0;
+        Console.WriteLine("nhap so tien gui: ");
+        money = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("nhap so thang gui: ");
+        month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Nhap lai suat");
+        interest_rate = Convert.ToDouble(Console.ReadLine());
+
+        double tongLai = 0;
+        for (int i = 0; i < month; i++)
+        {
+            tongLai = money * (interest_rate / 100) / 12 * 3;
+        }
+        Console.WriteLine("tong lai la: " + tongLai);
+    }
+}
