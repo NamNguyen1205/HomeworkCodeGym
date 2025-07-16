1e8836c67983e18b01b56f0bab16ca0888124ba0
nguyennam <nguyensonnam1205@gmail.com>
2025-07-16 22:01:01 +0700

    add
    add

---
+++ bai5_lap/b3_hienthi_sont/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hien thi cac so nguyen to");
+        for (var i = 2; i < 100; i++)
+        {
+            if (isPrime(i))
+            {
+                Console.WriteLine("" + i);
+            }
+        }
+    }
+    static bool isPrime(int number)
+    {
+        if (number < 2) return false;
+        for (var i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
