2631d6fb817cd8b71565177306dc2816e847b03e
nguyennam <nguyensonnam1205@gmail.com>
2025-07-21 22:00:19 +0700

    add
    add

---
+++ bai6_mang/bai4_themPT/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] Arr = { 1000, 23, 3, 8, 21, 100, 220 };
+        Console.WriteLine("Mang ban dau la: ");
+        PrintArray(Arr);
+        Console.WriteLine();
+
+        Console.WriteLine("Nhap so can chen: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        //
+        Console.WriteLine("Nhap vi tri can chen: ");
+        int position = Convert.ToInt32(Console.ReadLine());
+        //
+        if (position < 0 || position > Arr.Length) return;
+        int[] newArr = new int[Arr.Length + 1];
+        int i, j;
+        for (i = 0, j = 0; i <= Arr.Length; i++)
+        {
+            if (i == position)
+            {
+                newArr[i] = number;
+            }
+            else
+            {
+                newArr[i] = Arr[j];
+                j++;
+            }
+        }
+        PrintArray(newArr);
+
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + ", ");
+        }
+    }
+}
