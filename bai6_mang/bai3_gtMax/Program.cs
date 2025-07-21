16e6a22137bc5d3a7d68302ad43f857e8a0ef85b
nguyennam <nguyensonnam1205@gmail.com>
2025-07-21 21:45:46 +0700

    add
    add

---
+++ bai6_mang/bai3_gtMax/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1000, 23, 3, 8, 21, 100, 220 };
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (max <= numbers[i])
+            {
+                max = numbers[i];
+            }
+        }
+        Console.WriteLine("So lon nhat la: " + max);
+    }
+}
