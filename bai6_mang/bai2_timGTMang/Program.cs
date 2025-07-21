746a74ab58ec8bf4cc8681448143a0bd95ca6742
nguyennam <nguyensonnam1205@gmail.com>
2025-07-21 21:34:47 +0700

    add
    add

---
+++ bai6_mang/bai2_timGTMang/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] students = { "nam", "thanh", "nga", "thien", "hung", "long", "hoa", "mai", "manh", "kien" };
+        
+        Console.WriteLine("ten muon tim la: ");
+        string? cantim = Console.ReadLine();
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (cantim == students[i])
+            {
+                Console.WriteLine("vi tri thu: " + i);
+                
+                break;
+            }
+            if (i == students.Length - 1)
+            {
+                Console.WriteLine("ko co ten trong danh sach");
+            }
+        }
+    }
+}
