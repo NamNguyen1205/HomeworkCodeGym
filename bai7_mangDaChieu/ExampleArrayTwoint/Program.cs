43c6ad28c36077ef0a2f7d54ac9b8d9d2d7f8684
nguyennam <nguyensonnam1205@gmail.com>
2025-07-23 21:26:13 +0700

    add
    add

---
+++ bai7_mangDaChieu/ExampleArrayTwoint/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] matrix = new int[4, 3]
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {10, 11, 12},
+        };
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        //tinh tong dg cheo cua matrix
+        int sum2 = 0;
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (i == j) sum2 += matrix[i, j];
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("tong duong cheo la: " + sum2);
+        //////////////
+    }
+}
---
+++ bai7_mangDaChieu/b2_TimMax/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] matrix = new int[3, 3];
+        //Nhap ma tran
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"Nhap g/tri hang {i} cot {j}: ");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        //In ra ma tran da nhap
+        Console.WriteLine("Ma tran da nhap la: ");
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+        //Tim phan tu max
+        int max = matrix[0, 0];
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                if(matrix[i, j] >= max)
+                {
+                    max = matrix[i, j];
+                }
+            }
+        }
+        Console.WriteLine("Phan tu max la: " + max);
+
+        
+    }
+}
