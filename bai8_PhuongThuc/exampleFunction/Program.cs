87d52146f0bc0f51d49663b5336370ba67b53ae3
nguyennam <nguyensonnam1205@gmail.com>
2025-07-25 20:48:05 +0700

    add
    add

---
+++ bai8_PhuongThuc/exampleFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so nguyen a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap so nguyen b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int max = Max(a, b);
+        PrintMax(a,b);
+    }
+
+    public static int Max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+    public static void PrintMax(int a, int b)
+    {
+        Console.WriteLine("So lon nhat la: " + Max(a, b));
+    }
+}
