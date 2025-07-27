940e32c0561abb16a6190f02075a19b5f93bf75a
nguyennam <nguyensonnam1205@gmail.com>
2025-07-28 00:20:36 +0700

    add
    add

---
+++ bai8_PhuongThuc/b4_DemSoKT/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = "khong co gi la kho";
+        int count = FindChar(str, 'l');
+        Console.WriteLine($"So lan xuat hien cua 'o' trong chuoi: {count}");
+    }
+
+    public static int FindChar(string str, char c)
+    {
+        int count = 0;
+        foreach (char ch in str)
+        {
+            if (ch == c)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+}
