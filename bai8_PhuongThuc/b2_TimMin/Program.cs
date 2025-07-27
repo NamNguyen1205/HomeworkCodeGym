0326604adddd601b976d1a37a8c2d62da637ddd6
nguyennam <nguyensonnam1205@gmail.com>
2025-07-27 23:44:06 +0700

    add
    add

---
+++ bai8_PhuongThuc/b2_TimMin/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 5, 30, 81, 200, 44 };
+        int minValue = FindMin(numbers);
+        Console.WriteLine("The minimum value is: " + minValue);
+    }
+
+    public static int FindMin(int[] arr)
+    {
+        if (arr == null || arr.Length == 0)
+        {
+            Console.WriteLine("Mang ko ton tai hoac rong");
+        }
+
+        int min = arr[0];
+        foreach (int num in arr)
+        {
+            if (num < min)
+            {
+                min = num;
+            }
+        }
+        return min;
+    }
+}
