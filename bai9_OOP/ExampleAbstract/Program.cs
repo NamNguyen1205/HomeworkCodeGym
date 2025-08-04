c2e5e55d8820258e5f7d85fd0fd8db2487427dde
nguyennam <nguyensonnam1205@gmail.com>
2025-08-04 21:45:41 +0700

    add
    add

---
+++ bai9_OOP/ExampleAbstract/Program.cs
@@ -0,0 +1,20 @@
+﻿public abstract class BaseClass
+{
+    public abstract void AbstractMethod();
+}
+
+public class DerivedClass : BaseClass
+{
+    public override void AbstractMethod()
+    {
+        Console.WriteLine("Abstract method implemented in DerivedClass.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
