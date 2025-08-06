3f3c9b55226ce9d65d6aa4740e38cc6180539221
nguyennam <nguyensonnam1205@gmail.com>
2025-08-06 21:55:47 +0700

    add
    add

---
+++ bai10_kethua_dahinh/ExampleKethua/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Circle
+{
+    public double Radius { get; set; }
+    public Circle(double r)
+    {
+        Radius = r;
+    }
+    public virtual void Area()
+    {
+        Console.WriteLine("Calculating area of Circle");
+    }
+}
+
+public class Cylinder : Circle
+{
+    public double Height { get; set; }
+    public Cylinder(double r, double h) : base(r)
+    {
+        Height = h;
+    }
+    public override void Area()
+    {
+        Console.WriteLine("Calculating area of Cylinder");
+    }
+}
+
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Circle cyl = new Cylinder(5, 10);
+            cyl.Area();
+        }
+    }
