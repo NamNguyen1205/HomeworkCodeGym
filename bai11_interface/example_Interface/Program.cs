6f70c01933fea150c7edcb25a8e34c842eccf561
nguyennam <nguyensonnam1205@gmail.com>
2025-08-11 22:01:23 +0700

    add
    add

--- bai11_interface/example_Interface/Program.cs
+++ bai11_interface/example_Interface/Program.cs
@@ -1,7 +1,46 @@
-﻿public class Program
+﻿public class Player : IDamagable, IDestroy
+{
+    private int health;
+    public Player(int initialHealth)
+    {
+        health = initialHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health < 0) health = 0;
+    }
+    public void Dead()
+    {
+        if (health <= 0)
+        {
+            Console.WriteLine("Player is dead");
+        }
+        Destroy();
+    }
+
+    public void Destroy()
+    {
+        Console.WriteLine("Player has been destroyed");
+        health = 0;
+    }
+
+    public int Health => health;
+}
+
+
+
+public class Program
 {
     static void Main(string[] args)
     {
-        
+        Player player = new Player(100);
+        Console.WriteLine($"Initial Health: {player.Health}");
+
+        player.TakeDamage(10);
+        Console.WriteLine($"Initial Health: {player.Health}");
+
+        player.Dead();
+        Console.WriteLine($"Initial Health: {player.Health}");
     }
 }
