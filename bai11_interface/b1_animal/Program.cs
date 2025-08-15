4c4542f6cfaad69e34fdb1a15f6a633c3b61f88d
nguyennam <nguyensonnam1205@gmail.com>
2025-08-15 18:49:51 +0700

    add
    add

---
+++ bai11_interface/b1_animal/Program.cs
@@ -0,0 +1,35 @@
+﻿public abstract class Animal
+{
+    public abstract string MakeSound();
+}
+
+public class Tiger : Animal
+{
+    public override string MakeSound()
+    {
+        return "Tiger: roarrrrr!";
+    }
+}
+
+public class Chicken : Animal
+{
+    public override string MakeSound()
+    {
+        return "Chicken: cluck-cluck!";
+    }
+}
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    Animal[] animals = new Animal[2];
+    animals[0] = new Tiger();
+    animals[1] = new Chicken();
+
+    foreach (Animal animal in animals)
+    {
+      Console.WriteLine(animal.MakeSound());
+    }
+  }
+}
