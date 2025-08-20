96150f627b527e0fc278e87672d279bc18e95708
nguyennam <nguyensonnam1205@gmail.com>
2025-08-20 20:40:23 +0700

    add
    add

---
+++ bai13_File_Serialization/Example/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string SourceFilePath = "E:\\CodeGym\\File\\Source.txt";
+        string destinationPath = "E:\\CodeGym\\File\\Destination.txt";
+        // bool doesFileExist = File.Exists(destinationPath);
+        // File.Copy(SourceFilePath, destinationPath, true);
+        // Console.WriteLine($"File copied from {SourceFilePath} to {destinationPath}. File exist: {doesFileExist}");
+
+        // FileInfo sourceFile = new FileInfo(SourceFilePath);
+        // sourceFile.CopyTo(destinationPath, true);
+        // long fileSize = sourceFile.
+
+        //tạo file mới
+        string filePath = "E:\\FileNew";
+        //Directory.CreateDirectory(filePath);
+        DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+        directoryInfo.Create();
+    }
+}
---
+++ bai13_File_Serialization/b1_tinhtong/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "E:\\CodeGym\\HomeworkCodeGym\\bai13_File_Serialization\\b1_tinhtong\\file2.txt";
+        Console.WriteLine("File path: " + path);
+        //ReadTextFileExample example = new ReadTextFileExample();
+        ReadTextFile(path);
+    }
+    
+    static void ReadTextFile(string filePath)
+    {
+        try
+        {
+            FileInfo file = new FileInfo(filePath);
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+
+            StreamReader reader = new StreamReader(filePath);
+            string line = "";
+            int sum = 0;
+            while((line = reader.ReadLine()) != null) 
+            {
+                Console.WriteLine(line);
+                sum += Int32.Parse(line);
+            }
+            reader.Close();
+            Console.WriteLine("Total: " + sum);
+        }
+        catch (System.Exception)
+        {
+            Console.Error.WriteLine("File not found or invalid content");
+        }
+    }
+}
